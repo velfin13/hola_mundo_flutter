eb5073cdb35681a095adb2c768fdb5d352eb15f2
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-28 13:36:56 -0500

    separando la clase principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:hola_mundo/src/app.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-// ignore: use_key_in_widget_constructors
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Center(
-      child: Text("Hola mundo"),
-    ));
-  }
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(
+      child: Text("Hola mundo!!"),
+    ));
+  }
+}
