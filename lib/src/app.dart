9164dc4eecdb15ccd530ca5d3b093be0ae1fc497
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-28 19:02:30 -0500

    agregado varios botones

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import '/src/pages/contador_page.dart';
+// import 'pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -7,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Center(
-      child: HomePage(),
-    ));
+          child: ContadorPage(),
+        ));
   }
 }
---
+++ lib/src/pages/contador_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class ContadorPage extends StatefulWidget {
+  const ContadorPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ContadorPageState();
+}
+
+class _ContadorPageState extends State<ContadorPage> {
+  // ignore: prefer_final_fields
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("StatefulWidget"),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            "Numero de click",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "$_counter",
+            style: const TextStyle(fontSize: 40),
+          )
+        ],
+      )),
+      floatingActionButton: _crearBotones(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  Widget _crearBotones() {
+    return Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+      const SizedBox(
+        width: 15,
+      ),
+      FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.exposure_zero),
+      ),
+      const Expanded(child: SizedBox()),
+      FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.remove),
+      ),
+      const SizedBox(
+        width: 5,
+      ),
+      FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      const SizedBox(
+        width: 15,
+      ),
+    ]);
+  }
+}
